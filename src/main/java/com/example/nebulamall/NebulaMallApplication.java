c93017a0cb7dbc5b14aed19dcae6de98126abf86
zhaoyongze123 <zhaoyongze2023@163.com>
2026-01-31 21:40:40 +0800

    Nebula-Mall | 基于 Spring Cloud Alibaba 的高并发微服务电商系统

---
+++ src/main/java/com/example/nebulamall/NebulaMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.nebulamall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulaMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NebulaMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nebulamall/NebulaMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nebulamall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebulaMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
